26b72d25b84f4c72c27339c2621dbd6a6b73b325
jdscolam <jdscolam@fastmail.fm>
2015-09-12 21:41:44 -0500

    Initial Checkin

---
+++ Gint.Domain/Extensions/ArrayExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Gint.Domain.Extensions
+{
+    public static class ArrayExtensions
+    {
+        public static int SearchInt(this int[] sortedArray, int x)
+        {
+            while (true)
+            {
+                if (sortedArray.Length == 0)
+                    throw new ArgumentException("Array has no integers.");
+
+                if (x < sortedArray[0])
+                    return -1;
+
+                var midpoint = sortedArray.Length/2;
+                var midpointValue = sortedArray[midpoint];
+
+                if (midpointValue == x)
+                    return midpoint;
+
+                sortedArray = midpointValue > x ? sortedArray.Take(midpoint).ToArray() : sortedArray.Skip(midpoint).ToArray();
+            }
+        }
+    }
+}
---
+++ Gint.Tests/SearchingTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using Gint.Domain.Extensions;
+using NUnit.Framework;
+
+namespace Gint.Tests
+{
+    [TestFixture]
+    public class SearchingTests
+    {
+        [Test]
+        public void Search_through_sorted_array()
+        {
+            //Setup.
+            var random = new Random(DateTime.Now.Millisecond);
+            var sortedArray = Enumerable.Range(0, random.Next(5000)).ToArray();
+            var x = random.Next(sortedArray.Length + 1);
+
+            //Execute.
+            var index = sortedArray.SearchInt(x);
+
+            //Verify.
+            index.Should().BeGreaterOrEqualTo(0);
+
+            //Teardown.
+        }
+    }
+}
